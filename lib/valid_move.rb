3419b82d26d5f95ad39e6245d09e102ee96e3dce
Adam Napoletano <adam.napps@gmail.com>
2020-09-29 02:27:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(array, index_number)
 if index_number.between?(0,8)  && position_taken?(array,index_number)
-  falsey
+  false
 else
   true
 end
